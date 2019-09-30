ff55433294cec070d5db4a1ee49e8c02c6c0ad1d
cjm-1 <chris@mead.one>
2019-09-30 21:38:39 +0100

    Corrected QCAD version number to include all versions of QCAD 3 (3.x)

--- BrickArch.js
+++ BrickArch.js
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2019 by Christopher Mead. All rights reserved.
  *
- * This file is part of the Brick Arch project, an addon for QCAD 3.0.
+ * This file is part of the Brick Arch project, an addon for QCAD 3.x.
  *
  * This copy of Brick Arch is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public Licence as published by
